6641cd6ccafea3a157750ee35bc61887bd4e8778
Abdullah Hashim <imabusabah@users.noreply.github.com>
2017-08-31 09:20:25 +0100

    Verifies that guests are redirected to login page when making a post request to topics#create.

--- spec/requests/topics_spec.rb
+++ spec/requests/topics_spec.rb
@@ -5,6 +5,7 @@ describe 'Topic management', type: :request do
   let(:user) { FactoryGirl.create(:user) }
   let(:ayah) { FactoryGirl.create(:ayah) }
   let(:topic) { FactoryGirl.create(:topic) }
+  let(:topic_attributes) { FactoryGirl.attributes_for(:topic) }
 
   describe 'page access' do
 
@@ -33,6 +34,15 @@ describe 'Topic management', type: :request do
         end
         specify { expect(response).to be_successful }
         specify { expect(response.body).to include 'topic-form' }
+      end
+    end
+
+    describe 'POST topics#create' do
+      
+      # Guests should be redireted to the login page
+      context 'guest' do
+        before { post "/topics" }
+        specify { expect(response).to redirect_to new_user_session_path }
       end
     end
   end
