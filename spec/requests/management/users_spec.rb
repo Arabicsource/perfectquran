225f6bd01b20197438fa18eb5e993d5dce65a801
Abdullah Hashim <imabusabah@users.noreply.github.com>
2017-09-06 13:46:50 +0100

    Adds pundit gem.

--- spec/requests/management/users_spec.rb
+++ spec/requests/management/users_spec.rb
@@ -8,5 +8,13 @@ describe 'User management', type: :request do
       before { get manage_users_path }
       specify { expect(response).to redirect_to new_user_session_path }
     end
+
+    context 'non-admin user' do
+      before do
+        login_as FactoryGirl.create(:user, :confirmed)
+        get manage_users_path
+      end
+      specify { expect(response).to redirect_to root_path }
+    end
   end
 end
