e8895a3c9df8b8e0e38812c0f10a408f0b4febb0
Abdullah Hashim <imabusabah@users.noreply.github.com>
2017-09-16 12:32:19 +0100

    Refactores ayahs#show assignments into a facade.

--- app/controllers/ayahs_controller.rb
+++ app/controllers/ayahs_controller.rb
@@ -3,16 +3,6 @@
 # :nodoc:
 class AyahsController < ApplicationController
   def show
-    @surah = Surah.find(params[:surah_id])
-    @ayah = @surah.ayahs.find_by(number: params[:number])
-
-    return unless current_user
-
-    @memory = Memory.find_or_initialize_by(
-      ayah_id: @ayah.id, user_id: current_user.id
-    )
-    @favorite = Favorite.find_or_initialize_by(
-      ayah_id: @ayah.id, user_id: current_user.id
-    )
+    @ayah = AyahFacade.new(params[:surah_id], params[:number], current_user)
   end
 end
