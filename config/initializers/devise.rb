97a2d9b153f56eb643abc1fcb53ded9f32a2f1e5
Abdullah Hashim <imabusabah@users.noreply.github.com>
2017-09-18 17:17:22 +0100

    Changes devise sending email.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'The Perfect Quran <abdullah@meatgallery.co.uk>'
+  config.mailer_sender = 'The Perfect Quran <quran.is.perfect@gmail.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
