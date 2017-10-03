f33954fe842eeb419378368745b5522f8f2e34e4
Abdullah Hashim <imabusabah@users.noreply.github.com>
2017-10-03 10:44:03 +0100

    Removes pundit authorization gem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,5 @@
 
 # :nodoc:
 class ApplicationController < ActionController::Base
-  include Pundit
   protect_from_forgery with: :exception
-
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  private def user_not_authorized
-    redirect_to root_path
-  end
 end
