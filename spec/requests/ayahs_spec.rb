b1f01bbaef892319a4ba906c06beff42101ffa7c
Abdullah Hashim <imabusabah@users.noreply.github.com>
2017-10-16 19:24:06 +0100

    Removes unnecessary spec.

--- spec/requests/ayahs_spec.rb
+++ spec/requests/ayahs_spec.rb
@@ -4,20 +4,9 @@ require 'rails_helper'
 
 describe 'Ayahs' do
   let(:ayah) { FactoryGirl.create(:ayah) }
-  let(:user) { FactoryGirl.create(:user, :confirmed) }
 
   describe 'GET ayah_by_number_path' do
-    context 'geust' do
-      before { get ayah_by_number_path(ayah.surah.id, ayah.number) }
-      specify { expect(response).to be_successful }
-    end
-
-    context 'registered user' do
-      before do
-        login_as user
-        get ayah_by_number_path(ayah.surah.id, ayah.number)
-      end
-      specify { expect(response).to be_successful }
-    end
+    before { get ayah_by_number_path(ayah.surah.id, ayah.number) }
+    specify { expect(response).to be_successful }
   end
 end
