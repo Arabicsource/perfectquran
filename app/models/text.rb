3886e62784d8768204e2106b040ce15f11a136f5
Abdullah Hashim <imabusabah@users.noreply.github.com>
2017-10-21 23:19:29 +0100

    Styles quran/ayahs

--- app/models/text.rb
+++ app/models/text.rb
@@ -14,6 +14,8 @@
 class Text < ApplicationRecord
   searchkick
 
+  default_scope { order('id asc') }
+
   belongs_to :quran
   belongs_to :ayah
   scope :ordered_by_quran_id, -> { joins(:quran).order('qurans.id') }
