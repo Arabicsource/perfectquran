5410bfaff480d8c4b3c8ecdc0be3e562b14412eb
Abdullah Hashim <imabusabah@users.noreply.github.com>
2017-10-23 13:44:29 +0100

    Removes respond_to examples.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -17,8 +17,6 @@ RSpec.describe Category, type: :model do
   subject { FactoryGirl.build_stubbed(:category) }
 
   it { is_expected.to be_valid }
-  it { is_expected.to respond_to(:name) }
-  it { is_expected.to respond_to(:permalink) }
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to have_many :posts }
 
