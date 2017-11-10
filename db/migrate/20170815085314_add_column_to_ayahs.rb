58a090d5651b4c4492c64fdde91de2401350f6a8
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2017-11-10 10:40:31 +0000

    Reference Quran::Ayah instead of Ayah

--- db/migrate/20170815085314_add_column_to_ayahs.rb
+++ db/migrate/20170815085314_add_column_to_ayahs.rb
@@ -2,6 +2,6 @@ class AddColumnToAyahs < ActiveRecord::Migration[5.1]
   def change
     add_column :ayahs, :favorites_count, :integer, default: 0
 
-    Ayah.all.each { |a| Ayah.reset_counters(a.id, :favorites) }
+    Quran::Ayah.all.each { |a| Quran::Ayah.reset_counters(a.id, :favorites) }
   end
 end
