b68b9917408b77bc7acf721402dea3ac1eb25df2
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-01-17 08:09:28 +0000

    Updates look and feel of the registration form. Removes the hero and changes the redirect.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def after_sign_up_path_for(_resource)
-    edit_accounts_registrations_profile_path
+    accounts_profile_path
   end
 end
