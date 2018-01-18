e5214e09bbb63188938061ff4cbf281ba81fe95d
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-01-18 19:48:36 +0000

    Removes Facebook.

--- config/initializers/koala.rb
+++
@@ -1,5 +0,0 @@
-Koala.configure do |config|
-  config.access_token = Rails.application.secrets.facebook_access_token
-  config.app_id = Rails.application.secrets.facebook_app_id
-  config.app_secret = Rails.application.secrets.facebook_app_secret
-end
--- db/migrate/20171009160027_create_facebook_shares.rb
+++
@@ -1,9 +0,0 @@
-class CreateFacebookShares < ActiveRecord::Migration[5.1]
-  def change
-    create_table :facebook_shares do |t|
-      t.references :ayah, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
