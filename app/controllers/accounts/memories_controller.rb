94be9c90979e931eb7ef28b390f7859239ececd2
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-01-30 17:26:45 +0000

    Modifies memorization screens markup.

--- app/controllers/accounts/memories_controller.rb
+++ app/controllers/accounts/memories_controller.rb
@@ -7,6 +7,9 @@ module Accounts
       @memories = Memory.where(account: current_account)
       ayah_ids = @memories.map(&:ayah_id)
       @memory_count = Quran::Ayah.where(id: ayah_ids).sum(:character_length)
+      @not_memorized_surahs = current_account.not_memorized_surahs
+      @partially_memorized_surahs = current_account.partially_memorized_surahs
+      @memorized_surahs = current_account.memorized_surahs
     end
   end
 end
