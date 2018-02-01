5fc88495745d059ba0030a6270bab517b32f13b3
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-02-01 09:54:00 +0000

    Specs ayahs memories create action. Small refactoring to make code more readable.

--- app/controllers/accounts/memories/ayahs/memories_controller.rb
+++ app/controllers/accounts/memories/ayahs/memories_controller.rb
@@ -5,17 +5,13 @@ module Accounts
     module Ayahs
       # :nodoc:
       class MemoriesController < Accounts::BaseController
+        before_action :load_resources
+
         def create
-          @ayah = Quran::Ayah.find_by(id: params[:ayah_id])
-          @surah = @ayah.surah
           @memory = Memory.new(ayah: @ayah, account: current_account)
 
           if @memory.save
-            ayah_ids = Memory.where(
-              account: current_account, ayah_id: @surah.ayah_ids
-            ).map(&:ayah_id)
-            @memory_count = Quran::Ayah.where(id: ayah_ids)
-                                       .sum(:character_length)
+            @memory_count = memory_count
             render :create
           else
             render json: 'unprocessible entity', status: :unprocessable_entity
@@ -23,17 +19,27 @@ module Accounts
         end
 
         def destroy
-          @ayah = Quran::Ayah.find_by(id: params[:ayah_id])
-          @surah = @ayah.surah
           @memory = Memory.find_by(ayah: @ayah, account: current_account)
 
           @memory.try(:delete)
+          @memory_count = memory_count
+          render :destroy
+        end
+
+        private
+
+        def memory_count
           ayah_ids = Memory.where(
             account: current_account, ayah_id: @surah.ayah_ids
           ).map(&:ayah_id)
+
           @memory_count = Quran::Ayah.where(id: ayah_ids)
-                                      .sum(:character_length)
-          render :destroy
+                                     .sum(:character_length)
+        end
+
+        def load_resources
+          @ayah = Quran::Ayah.find_by(id: params[:ayah_id])
+          @surah = @ayah.surah
         end
       end
     end
---
+++ spec/requests/accounts/memories/ayahs/create_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'POST /accounts/memory/ayahs', type: :request do
+  let(:account) { create :account }
+  let(:ayah) { create :ayah }
+  let(:valid_url) { "/accounts/memory/ayahs/#{ayah.id}/memory.js" }
+
+  context 'without account' do
+    it 'returns unauthorized' do
+      post valid_url
+
+      expect(response).to be_unauthorized
+    end
+
+    it 'does not change memory count' do
+      expect { post valid_url }.not_to change(Memory, :count)
+    end
+  end
+
+  context 'with account and successful' do
+    before { login_as account }
+
+    it 'returns successfully' do
+      post valid_url
+
+      expect(response).to be_success
+    end
+
+    it 'changes memory count' do
+      expect { post valid_url }.to change(Memory, :count).by(1)
+    end
+  end
+end
