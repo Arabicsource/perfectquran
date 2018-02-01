3bcd000e82b1ab99df4d83dfb760f4cf1ff67ef3
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-02-01 14:49:41 +0000

    Refactors Hifz manager to remove duplication and to make there be only one loop to group each surah into not memorized, partially memorized or completely memorized.

--- app/services/hifz_manager.rb
+++ app/services/hifz_manager.rb
@@ -6,6 +6,9 @@ class HifzManager
     @account = account
     @memories = Memory.where(account: account)
     @ayah_ids = @memories.map(&:ayah_id)
+    @not_memorized_surahs = []
+    @partially_memorized_surahs = []
+    @memorized_surahs = []
   end
 
   def memory_count
@@ -13,43 +16,40 @@ class HifzManager
   end
 
   def not_memorized_surahs
-    not_memorized_surahs = []
-
-    Quran::Surah.all.each do |surah|
-      ayah_ids = surah.ayahs.pluck :id
-      memory_count = Memory.where(ayah: ayah_ids, account: @account).count
-
-      not_memorized_surahs << surah if memory_count.zero?
-    end
-
-    not_memorized_surahs
+    group_surahs unless surahs_grouped?
+    @not_memorized_surahs
   end
 
   def partially_memorized_surahs
-    partially_memorized_surahs = []
-
-    Quran::Surah.all.each do |surah|
-      ayah_ids = surah.ayahs.pluck :id
-      memory_count = Memory.where(ayah: ayah_ids, account: @account).count
-
-      if memory_count.positive? && memory_count < surah.number_of_ayahs
-        partially_memorized_surahs << surah
-      end
-    end
-
-    partially_memorized_surahs
+    group_surahs unless surahs_grouped?
+    @partially_memorized_surahs
   end
 
   def memorized_surahs
-    memorized_surahs = []
+    group_surahs unless surahs_grouped?
+    @memorized_surahs
+  end
 
+  private
+
+  def surahs_grouped?
+    @not_memorized_surahs.any? ||
+    @partially_memorized_surahs.any? ||
+    @memorized_surahs.any?
+  end
+
+  def group_surahs
     Quran::Surah.all.each do |surah|
       ayah_ids = surah.ayahs.pluck :id
       memory_count = Memory.where(ayah: ayah_ids, account: @account).count
 
-      memorized_surahs << surah if memory_count >= surah.number_of_ayahs
+      if memory_count.zero?
+        @not_memorized_surahs << surah
+      elsif memory_count >= surah.number_of_ayahs
+        @memorized_surahs << surah
+      else
+        @partially_memorized_surahs << surah
+      end
     end
-
-    memorized_surahs
   end
 end
