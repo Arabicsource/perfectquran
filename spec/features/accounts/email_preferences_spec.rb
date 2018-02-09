e29585cb15737f3e0a334977ab5dc3a376744287
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-02-09 14:36:48 +0000

    Updates html structure and style of email preferences.

--- spec/features/accounts/email_preferences_spec.rb
+++ spec/features/accounts/email_preferences_spec.rb
@@ -8,12 +8,12 @@ feature 'Email Preferences' do
 
   background { login_as account }
 
-  scenario 'for account' do
+  scenario 'for newly created account' do
     visit account_root_path
 
     click_on 'accounts-navbar-settings-link'
     click_on 'Email Preferences'
 
-    expect(page).to have_field('account_email_preferences_general', checked: true)
+    expect(page).to have_field('account_email_preference_general', checked: true)
   end
 end
