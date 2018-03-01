a80771fdd8d39521e74cf54f379e6f1b261bba79
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-03-01 21:52:24 +0000

    Updates charges index.

---
+++ test/conrollers/account/charges_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class Account::ChargesControllerTest < ActionDispatch::IntegrationTest
+  test 'get index without account' do
+    get account_charges_path
+    assert_redirected_to new_account_session_path
+  end
+
+  test 'get index with account' do
+    login_as accounts(:abdullah)
+    get account_charges_path
+    assert_response :success
+  end
+
+  test 'get index with account on mobile' do
+    login_as accounts(:abdullah)
+    mobile_browser
+    get account_charges_path
+    assert_response :success
+  end
+end
