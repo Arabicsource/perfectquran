b2376c429315fd58b051bd6e0a1e93c8e673befc
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-03-01 15:11:02 +0000

    Delete migtation

--- db/migrate/20180301144727_bookmarkable.rb
+++
@@ -1,6 +0,0 @@
-class Bookmarkable < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :bookmarks, :ayah_id, :bookmarkable_id
-    add_column :bookmarks, :bookmarkable_type, :string
-  end
-end
