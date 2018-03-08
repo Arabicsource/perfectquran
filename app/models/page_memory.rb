289d853ad636c6f731b0bcb8c1d5518d8a30d162
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-03-08 21:50:51 +0000

    Page count is update on memory total when page is memorized.

--- app/models/page_memory.rb
+++ app/models/page_memory.rb
@@ -1,4 +1,16 @@
 class PageMemory < ApplicationRecord
   belongs_to :account
   belongs_to :page
+
+  validates :page_id, uniqueness: { scope: :account_id }
+
+  after_save :update_memory_total_page_count
+
+  private
+
+  def update_memory_total_page_count
+    return unless character_length == page.character_length
+    memory_total = MemoryTotal.find_by(account: account)
+    memory_total.update_attribute(:page_count, memory_total.page_count + 1)
+  end
 end
