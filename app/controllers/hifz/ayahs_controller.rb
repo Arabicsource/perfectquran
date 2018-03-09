4e04190ac63d083e33fcd8b8970b342b088d864c
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-03-09 00:48:26 +0000

    Hifs ayahs show.

---
+++ app/controllers/hifz/ayahs_controller.rb
@@ -0,0 +1,10 @@
+module Hifz
+  class AyahsController < ApplicationController
+    def show
+       @page = Page.find(params[:page_id])
+       @page.current_ayah = Ayah.find(params[:id])
+
+       render 'hifz/pages/show'
+    end
+  end
+end
