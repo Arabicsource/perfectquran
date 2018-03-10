946ea6b4e041ae232f63486d585bcf227e1d0a9b
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-03-10 12:18:02 +0000

    Removes duplicated tests.

--- test/models/juz_test.rb
+++ test/models/juz_test.rb
@@ -5,19 +5,6 @@ require 'test_helper'
 class JuzTest < ActiveSupport::TestCase
   setup { @juz = juzs(:juz_1) }
 
-  test 'ayahs' do
-    assert_equal (1..148).to_a, juzs(:juz_1).ayahs.map(&:id)
-  end
-
-  test 'pages' do
-    assert_equal (1..21).to_a, juzs(:juz_1).pages.map(&:id)
-  end
-
-  test 'first_page' do
-    assert_equal pages(:page_1), @juz.first_page
-    assert_equal pages(:page_22), juzs(:juz_2).first_page
-  end
-
   test 'memorized?' do
     Current.account = accounts(:abdullah)
     juz = juzs(:juz_1)
--- test/models/surah_test.rb
+++ test/models/surah_test.rb
@@ -3,10 +3,6 @@
 require 'test_helper'
 
 class SurahTest < ActiveSupport::TestCase
-  test 'pages' do
-    assert_equal [1], surahs(:surah_1).pages.map(&:id)
-  end
-
   test 'description' do
     surah = surahs(:surah_1)
     assert_equal 'The Opening - الفاتحة', surah.description
@@ -43,15 +39,5 @@ class SurahTest < ActiveSupport::TestCase
 
     surahs(:surah_1).memorize
     assert_equal 7, Current.account.memories.count
-  end
-
-  test 'first_page' do
-    assert_equal pages(:page_1), surahs(:surah_1).first_page
-    assert_equal pages(:page_2), surahs(:surah_2).first_page
-  end
-
-  test 'first_ayah' do
-    assert_equal ayahs(:ayah_1), surahs(:surah_1).first_ayah
-    assert_equal ayahs(:ayah_8), surahs(:surah_2).first_ayah
   end
 end
