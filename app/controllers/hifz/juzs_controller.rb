849edc162fad53813400e7e2e642f217907c9280
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-04-05 15:37:32 +0100

    Removes hizf controllers and views.

--- app/controllers/hifz/juzs_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Hifz
-  class JuzsController < ApplicationController
-    def index
-      @juzs = Quran::Juz.all
-    end
-  end
-end
--- app/controllers/hifz/surahs_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Hifz
-  class SurahsController < ApplicationController
-    def index
-      @surahs = Quran::Surah.all
-    end
-  end
-end
