ee82a5a6a02a0bb3c3f6a36188217d16c1c0f396
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-04-08 08:23:08 +0100

    Adds association specs.

--- spec/models/account_translation_spec.rb
+++ spec/models/account_translation_spec.rb
@@ -3,5 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe AccountTranslation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to :account }
+    it { is_expected.to belong_to :translation }
+  end
 end
