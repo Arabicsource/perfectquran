c4196cfadd0751a5e316a70b8adeb9ecb95492c0
Abdullah Hashim <abdullah.slice.chop@gmail.com>
2018-04-25 17:28:02 +0100

    Fixes links to ayahs#show.

--- spec/features/quran/browsing_spec.rb
+++ spec/features/quran/browsing_spec.rb
@@ -7,16 +7,44 @@ feature 'Browsing' do
 
   scenario 'surah' do
     visit root_path
-    click_on 'Quran'
 
+    click_on 'Quran'
     expect(page).to have_title 'Surahs'
 
     click_on 'Al-Fatihah'
+    expect(page).to have_title 'Al-Fatihah'
 
+    click_on 'Next Surah'
+    expect(page).to have_title 'Al-Baqarah'
+
+    click_on 'Previous'
+    expect(page).to have_title 'Al-Fatihah'
+
+    click_on 'Previous'
+    expect(page).to have_title 'An-Nas'
+
+    click_on 'Next Surah'
     expect(page).to have_title 'Al-Fatihah'
 
     click_on '1'
+    expect(page).to have_title 'Al-Fatihah 1'
 
+    click_on 'Next Ayah'
+    expect(page).to have_title 'Al-Fatihah 2'
+
+    click_on 'Previous'
+    expect(page).to have_title 'Al-Fatihah 1'
+
+    click_on 'Previous'
+    expect(page).to have_title 'An-Nas 6'
+
+    click_on 'Next Ayah'
+    expect(page).to have_title 'Al-Fatihah 1'
+
+    click_on '(1:2)'
+    expect(page).to have_title 'Al-Fatihah 2'
+
+    click_on '(1:1)'
     expect(page).to have_title 'Al-Fatihah 1'
   end
 end
